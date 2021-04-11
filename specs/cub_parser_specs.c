233fad61f1585990e5cdb268605aee41af97b4ab
Jules Rivoire <julesriv@gmail.com>
2021-04-11 17:34:01 +0200

    [cub_parser] testing the map as well

--- specs/cub_parser_specs.c
+++ specs/cub_parser_specs.c
@@ -22,23 +22,15 @@ void	print_diff(char *what, char *str_result, char *str_expected, int result, in
 	}
 }
 
-static int comp_string(char *str1, char *str2)
-{
-	if (!str1 && !str2)
-		return (0);
-	if (!str1 || !str2)
-		return (1);
-	return (strcmp(str1, str2));
-}
-
 static void	check_result(t_des *result, t_des *expected, int *sucess, int *failure)
 {
 	int fail = 0;
 	if ((expected && !result) ||(!expected && result) )
 	{
 		printf(RED "KO\n" RESET);
-		printf("Expected = [%p]\n", expected);
-		printf("Got      = [%p]\n", result);
+		printf(UNDER RED "Pointer :\n" RESET);
+		printf("Expected = %s\n", (expected ? "Something" : "Nothing"));
+		printf("Got      = %s\n", (result ? "Something" : "Nothing"));
 		fail++;
 	}
 	if (expected && result)
@@ -62,9 +54,12 @@ static void	check_result(t_des *result, t_des *expected, int *sucess, int *failu
 		if (result->ceiling_c != expected->ceiling_c)
 			print_diff("Ceiling colour", NULL, NULL, result->ceiling_c, expected->ceiling_c, fail++, 3);
 	}
-	if (fail == 0)
-		printf(GREEN "OK\n" RESET);
-	fail == 0 ? (*sucess)++ : (*failure)++;
+	if (fail != 0)
+	{
+		(*failure)++;
+		return ;
+	}
+	check_map(*result, *expected, sucess, failure);
 }
 
 void free_result(t_des *description)
@@ -76,10 +71,11 @@ void free_result(t_des *description)
 	free(description->so_path);
 	free(description->we_path);
 	free(description->s_path);
+	free_map(description->map);
 	free(description);
 }
 
-static void texture_parser_test(char *describe, char *path, int returned, t_des *expected, int *sucess, int *failure)
+static void cub_parser_test(char *describe, char *path, int returned, t_des *expected, int *sucess, int *failure)
 {
 	int response;
 	int fd;
@@ -107,6 +103,7 @@ static void texture_parser_test(char *describe, char *path, int returned, t_des 
 		(*failure)++;
 	}
 	free_result(description);
+	free_map(expected->map);
 	close(fd);
 	return ;
 }
@@ -117,22 +114,24 @@ int main(void)
 	int sucess = 0;
 	int failure = 0;
 
-	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", 0xFFFFFF, 0x000000);
-	texture_parser_test("Simple well formated file", "files/well_formated.cub", 0, &expected, &sucess, &failure);
-	texture_parser_test("Well formated file shuffled", "files/well_formated_shuffled.cub", 0, &expected, &sucess, &failure);
-	texture_parser_test("Well formated with empty lines", "files/WF_with_empty_lines.cub", 0, &expected, &sucess, &failure);
-	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", -1, -2);
-	texture_parser_test("Floor color wrong format", "files/invalid_floor_c.cub", 1, &expected, &sucess, &failure);
-	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "|duplicate|", NULL, -2, -2);
-	texture_parser_test("Duplicate west path", "files/duplicate_we_path.cub", 1, &expected, &sucess, &failure);
-	expected = filled_t_des(-1, -1, NULL, NULL, NULL, NULL, NULL, -2, -2);
-	texture_parser_test("Resolution in the wrong format", "files/wrong_format_resolution.cub", 1, &expected, &sucess, &failure);
-	expected = filled_t_des(0, 0, NULL, NULL, NULL, NULL, NULL, -2, -2);
-	texture_parser_test("Wrong identifier at start", "files/unknown_identifier.cub", 1, &expected, &sucess, &failure);
-	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", 0xFFFFFF, -2);
-	texture_parser_test("Missing element", "files/missing_ceiling_c.cub", 1, &expected, &sucess, &failure);
-	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", -2, -2);
-	texture_parser_test("Format ok, until line with a few space chars but then random", "files/with_line_starting_space_then_random.cub", 1, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", 0xFFFFFF, 0x000000, "1111>1001>1W01>1111");
+	cub_parser_test("Simple well formated file", "files/well_formated.cub", 0, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", 0xFFFFFF, 0x000000, "1111>1001>1W01>1111");
+	cub_parser_test("Well formated file shuffled", "files/well_formated_shuffled.cub", 0, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", 0xFFFFFF, 0x000000, "1111>1001>1W01>1111");
+	cub_parser_test("Well formated with empty lines", "files/WF_with_empty_lines.cub", 0, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", -1, -2, NULL);
+	cub_parser_test("Floor color wrong format", "files/invalid_floor_c.cub", 1, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "|duplicate|", NULL, -2, -2, NULL);
+	cub_parser_test("Duplicate west path", "files/duplicate_we_path.cub", 1, &expected, &sucess, &failure);
+	expected = filled_t_des(-1, -1, NULL, NULL, NULL, NULL, NULL, -2, -2, NULL);
+	cub_parser_test("Resolution in the wrong format", "files/wrong_format_resolution.cub", 1, &expected, &sucess, &failure);
+	expected = filled_t_des(0, 0, NULL, NULL, NULL, NULL, NULL, -2, -2, NULL);
+	cub_parser_test("Wrong identifier at start", "files/unknown_identifier.cub", 1, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", 0xFFFFFF, -2, NULL);
+	cub_parser_test("Missing element", "files/missing_ceiling_c.cub", 1, &expected, &sucess, &failure);
+	expected = filled_t_des(1900, 1800, "./a/no_path.c", "./a/ea_path.c", "./a/so_path.c", "./a/we_path.c", "./a/s_path.c", -2, -2, NULL);
+	cub_parser_test("Format ok, until line with a few space chars but then random", "files/with_line_starting_space_then_random.cub", 1, &expected, &sucess, &failure);
 
 	printf("\t%d success out of %d tests\n", sucess, (sucess + failure));
 	return (0);
